142cbc761c399ebb64fcb92bddd8340565b8dfc3
Ben Barden <628824+benbarden@users.noreply.github.com>
2019-09-16 20:55:33 +0100

    Upgrading to Bootstrap 4; adding basic homepage content

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,10 +8,32 @@ class WelcomeController extends Controller
 {
     public function show()
     {
+        $randomFooters = [
+            'a very angry shiny Pidgey',
+            'a hungry Alolan Rattata',
+            'a flower crown Eevee that looks at you a bit funny',
+            'a shadow Snorlax who\'s been dieting and is extra hungry',
+            'a level 1 Spearow with a devilish peck',
+            'a rabid Bulbasaur',
+            'a drunk shiny Timmy',
+            'Colonel Binface',
+            'a maxed shiny Rayquaza in need of a cuddle',
+            'Mr. Mime in a leotard',
+            'an extra large level 30 shiny Magikarp',
+            'a Delibird looking for its Santa hat',
+            'a Swinub on a unicycle',
+            'a Lunatone floating aimlessly in the middle of the day',
+            'a shiny lucky hundo flower crown Flareon with 12 legs',
+            'a lemon meringue Caterpie',
+            'a yellow Snover',
+        ];
+
         $bindings = [];
 
-        $bindings['TopTitle'] = 'Coming soon';
-        $bindings['PageTitle'] = 'Coming soon';
+        $bindings['TopTitle'] = 'Welcome';
+        $bindings['PageTitle'] = 'Welcome';
+
+        $bindings['RandomFooters'] = $randomFooters;
 
         return view('welcome', $bindings);
     }
